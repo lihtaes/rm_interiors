eff2e67eb1c5b7706f0659f3b458e07dd9f792a0
Bradley Campbell <bb.campbell606@gmail.com>
2020-09-26 13:48:57 -0700

    configured/created db, added entity directory, created job and tile entities

---
+++ src/AppBundle/Entity/Job.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Job {
+    private $start_date;
+    private $owner;
+    private $cost;
+    private $tile;
+}
+
+?>
---
+++ src/AppBundle/Entity/Tile.php
@@ -0,0 +1,11 @@
+<?php 
+namespace AppBundle\Entity;
+
+class Tile {
+    private $variety;
+    private $color;
+    private $quantity_sqft;
+    private $price_per_sqft;
+}
+
+ ?>
