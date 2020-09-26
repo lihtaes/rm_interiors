8a6aafbf0042fe9366bba0d1a83593d2ed819181
Bradley Campbell <bb.campbell606@gmail.com>
2020-09-26 14:06:12 -0700

    added ORM mapping for entities by annotation

--- src/AppBundle/Entity/Job.php
+++ src/AppBundle/Entity/Job.php
@@ -2,11 +2,31 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 class Job {
-    private $start_date;
-    private $owner;
-    private $cost;
-    private $tile;
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $job_owner;
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $total_sq_ft;
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    private $final_bid;
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $tile_variety;
 }
 
 ?>
--- src/AppBundle/Entity/Tile.php
+++ src/AppBundle/Entity/Tile.php
@@ -1,10 +1,30 @@
 <?php 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 class Tile {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     private $variety;
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
     private $color;
-    private $quantity_sqft;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantity_in_sqft;
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
     private $price_per_sqft;
 }
 
