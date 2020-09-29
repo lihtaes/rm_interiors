f572db1e9cc7ab63abbaf6d64a86c68d9bbb8426
Bradley Campbell <bb.campbell606@gmail.com>
2020-09-29 12:58:51 -0700

    added routing for job controller

--- src/AppBundle/Controller/JobController.php
+++ src/AppBundle/Controller/JobController.php
@@ -8,14 +8,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Job {
 
+    /**
+     * @Route("/job")
+     */
     public function indexAction(){
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
     }
-
+    /**
+     * @Route("/job/new")
+     */
     public function createAction(Request $request, $job_owner, $total_sq_ft, $final_bid, $tile_variety) {
+
+        
+
         $job = new Job();
         $job->setJobOwner($job_owner);
         $job->setTotalSqFt($total_sq_ft);
